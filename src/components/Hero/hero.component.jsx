3c08ac677cc713d030773bc77c816979b53384da
grsakshi <sakshi.g642001@gmail.com>
2021-10-10 22:15:41 +0530

    changed ui of hero component

--- src/components/Hero/hero.component.jsx
+++ src/components/Hero/hero.component.jsx
@@ -8,14 +8,14 @@ const Hero = () => (
                     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Hey! This is the homepage
                         <br class="hidden lg:inline-block"/>Get ready
                     </h1>
-                    <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                    <p class="mb-8 leading-relaxed">This website is for a hacktober fest task. This is a multipage website created using react and tailwind.</p>
                     <div class="flex justify-center">
-                        <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
-                        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+                        <button class="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Know More</button>
+                        <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Login</button>
                     </div>
                 </div>
                 <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                    <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600" />
+                    <img class="object-cover object-center rounded-3xl" alt="hero" src="bird.jpg" />
                 </div>
             </div>
         </section>
